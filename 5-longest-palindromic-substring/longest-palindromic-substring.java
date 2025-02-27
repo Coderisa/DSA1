081732b22a1baa4380131094a7db9f013bb51159
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-02-27 18:34:43 +0530

    Time: 237 ms (23.28%) | Memory: 56.3 MB (5.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,29 +1,41 @@
 class Solution {
-    boolean Solve(String s,int i,int j) {      
-            if(i>=j)
-             return true;
-            if(s.charAt(i)==s.charAt(j))
-               return Solve(s,i+1,j-1);
-            else
-               return false;   
-        }
+    private int[][] t;
+
+    int Solve(String s, int i, int j) {      
+        if (i >= j)
+            return 1;
+        if (t[i][j] != -1)
+            return t[i][j];
+
+        if (s.charAt(i) == s.charAt(j))
+            return t[i][j] = Solve(s, i + 1, j - 1);
+        else
+            return t[i][j] = 0;
+    }
+
     public String longestPalindrome(String s) {
-       int n=s.length();
-        //Check Palindrome string fn 
-     
-        int maxLen=0;
-        int sp=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                  
-                  if (Solve(s,i,j)==true){
-                    if(j-i+1>maxLen){
-                        maxLen=j-i+1;
-                        sp=i;
-                    }
-                  }
+        int n = s.length();
+        t = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                t[i][j] = -1;
             }
         }
-        return s.substring (sp,maxLen+sp);
+
+        int maxLen = 0;
+        int sp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (Solve(s, i, j) == 1) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        sp = i;
+                    }
+                }
+            }
+        }
+
+        return s.substring(sp, maxLen + sp);
     }
 }
