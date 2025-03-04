c9362aeb421e59b798687807c4c544d226829504
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-05 00:33:49 +0530

    Time: 34 ms (20.74%) | Memory: 45.4 MB (46.12%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         //bina ek baar traverse kare bina to mughe nhi pta chale ga ki kon sa elemnt aisa hai jpo repeat nhi hua hai
         HashMap<Character,Integer>mp=new HashMap <>();
         for(int i=0;i<s.length();i++)
-          mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);//0 ka matlb hai ki sirf ek baar aya hai
+          mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);//1 ka matlb hai ki sirf ek baar aya hai
         for(int i=0;i<s.length();i++){
           if (mp.get(s.charAt(i))==1)  
           return i;}
