bfce6aa81101f9f761e3decb726dedd65065f1a9
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-05 02:44:59 +0530

    Time: 31 ms (52.79%) | Memory: 67.7 MB (31.30%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+
+                longest = Math.max(longest, length);
+            }
+        }
+
+        return longest;        
+    }
+}
+/*class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashMap <Integer,Integer>mp=new HashMap<>();
+        for(int i:nums)
+           mp.put(i,mp.getOrDefault(i,0)+1);
+        int max=0;   //nums =[]
+
+        for(int i:nums){
+            if(!mp.containsKey(i-1)){
+              int count=1;
+              while(mp.containsKey(i+count)){
+                count++;
+                mp.put(i,mp.get(i)-1);}
+              max=Math.max(max,count);  
+
+        }  
+        }
+        return max;
+    }
+}*/
+
+
+      /*  HashMap <Integer,Boolean>mp=new HashMap<>();
+        for(int i:nums)
+           mp.put(i,true);
+        for(int i:nums){
+          if (mp.containsKey(i-1))
+              mp.put(i,false);
+        }     
+        int msp=0;int ml=0;
+        for(int i:nums){
+          
+            if(mp.get(i)==true){
+               int tl=1;
+               
+               while(mp.containsKey(i+tl))
+                  tl++;
+               if(tl>ml){
+               ml=tl;
+               msp=i;}   
+        }
+        }
+        return ml;
+    }
+}*/
