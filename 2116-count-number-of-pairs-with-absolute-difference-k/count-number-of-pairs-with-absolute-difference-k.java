dee7e693b5e372c5b7be0654d94adeb521a90034
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-07 00:38:46 +0530

    Time: 4 ms (93.90%) | Memory: 44.5 MB (13.35%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -5,8 +5,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if (mp.containsKey(nums[i]-k))
               c+=mp.get(nums[i]-k);
-            if (mp.containsKey(nums[i]+k)){
-              c+=mp.get(nums[i]+k);}
+            if (mp.containsKey(nums[i]+k))
+              c+=mp.get(nums[i]+k);
             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
         return c;
