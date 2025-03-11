e4e4032c44976c051a480cca82cbf787ad203ee5
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-11 12:08:14 +0530

    Time: 15 ms (38.80%) | Memory: 44.5 MB (79.55%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -4,20 +4,20 @@ class Solution {
 
         int result = 0;
 
-        int[] mp = new int[3]; // 0 - a, 1 - b, 2 - c
+        int[] mp = new int[3]; // 0 - a, 1 - b, 2 - c -->java automatically assigns 0 
 
         int i = 0;
         int j = 0;
 
         while (j < n) {
             char ch = s.charAt(j);
-            mp[ch - 'a']++;
+            mp[ch - 'a']++; //a-97,b-98,c-99
 
             while (mp[0] > 0 && mp[1] > 0 && mp[2] > 0) {
                 result += (n - j);
 
                 mp[s.charAt(i) - 'a']--;
-                i++;
+                i++;//moving the sliding window ahead
             }
 
             j++;
