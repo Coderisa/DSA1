3e9cf7f1acdade9a76d5ce4c67d4ce51f16289be
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-13 00:56:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (63.30%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,42 +1,43 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        int firstPos = nums.length - firstPositive(nums); // Count of positive numbers
-        int lastN = lastNegative(nums) + 1;              // Count of negative numbers
-
-        // Handle edge cases
-        if (firstPositive(nums) == -1) firstPos = 0; // No positive numbers
-        if (lastNegative(nums) == -1) lastN = 0;    // No negative numbers
-
-        return Math.max(firstPos, lastN);
+        int lastnegIndex= lastNegative(nums) +1 ;
+        int firtposIndex = nums.length - firstPositive(nums);
+        return Math.max(lastnegIndex , firtposIndex);
     }
 
-    int lastNegative(int[] nums) {
-        int s = 0, e = nums.length - 1;
-        int ans = -1;
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            if (nums[mid] < 0) {
-                ans = mid;
-                s = mid + 1; // Search the right part
-            } else {
-                e = mid - 1; // Search the left part
+    public int lastNegative(int[] nums){
+        int start = 0 ;
+        int end = nums.length-1 ;
+        int ans = -1 ;
+
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid]<0){
+                ans = mid ;
+                start = mid+1 ;
+            }else{
+                end = mid-1 ;
             }
         }
-        return ans;
+
+        return ans ;
     }
 
-    int firstPositive(int[] nums) {
-        int s = 0, e = nums.length - 1;
-        int ans = -1;
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            if (nums[mid] > 0) {
-                ans = mid;
-                e = mid - 1; // Search the left part
-            } else {
-                s = mid + 1; // Search the right part
+    public int firstPositive(int[] nums){
+        int start = 0 ;
+        int end = nums.length-1;
+        int ans = nums.length;
+
+        while(start<=end){
+            int mid = (start + end )/2;
+            if(nums[mid]>0){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start = mid+1 ;
             }
         }
-        return ans;
+
+        return ans ;
     }
 }
