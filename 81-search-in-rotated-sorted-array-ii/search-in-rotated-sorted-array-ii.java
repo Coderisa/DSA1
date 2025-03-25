2a732d3bdfb87096dbc0044cf296143b2505df70
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-25 12:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (50.24%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public boolean search(int[] nums, int target) {
         int b=0,e=nums.length-1;
         //metod 1 from pepcoding here we find mid and check ki kon sa region sorted hai.start to mid ya after mid ek to ho ga hi
@@ -21,4 +22,48 @@ class Solution {
         }
         return false;
     }
+}*/
+//method 2-find pivot method
+public boolean binarysearch(int[] nums, int b, int e, int target) {
+    while (b <= e) {
+        int mid = b + (e - b) / 2; // Corrected mid calculation
+        if (nums[mid] == target) {
+            return true;
+        }
+        if (nums[mid] < target) {
+            b = mid + 1;
+        } else {
+            e = mid - 1;
+        }
+    }
+    return false;
+}
+
+public boolean search(int[] nums, int target) {
+    int n = nums.length;
+    int p = pivot(nums, 0, n - 1);
+    // Search in the two portions divided by the pivot
+    if (binarysearch(nums, 0, p - 1, target)) {
+        return true;
+    }
+    return binarysearch(nums, p, n - 1, target);
+}
+
+int pivot(int[] nums, int b, int e) {
+    while (b < e) {
+        while (b < e && nums[b] == nums[b + 1]) {
+            b++;
+        }
+        while (b < e && nums[e] == nums[e - 1]) {
+            e--;
+        }
+        int mid = b + (e - b) / 2; // Corrected mid calculation
+        if (nums[mid] <= nums[e]) { // Sorted part
+            e = mid; // Possibly my pivot
+        } else {
+            b = mid + 1;
+        }
+    }
+    return e; // Pivot index
+}
 }
