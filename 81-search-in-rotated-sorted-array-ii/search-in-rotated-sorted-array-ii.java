ac781173709b81d89aebba06397ff387fdaf38d6
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-03-25 11:41:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (10.63%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         int b=0,e=nums.length-1;
+        //metod 1 from pepcoding here we find mid and check ki kon sa region sorted hai.start to mid ya after mid ek to ho ga hi
+        //then dorted region mein we check for our eleemnt and shift boundary
         while(b<=e){
-            while(b<e &&nums[b]==nums[b+1]) b++;
-            while(b<e && nums[e]==nums[e-1])e--;
+            while(b<e &&nums[b]==nums[b+1]) b++;//to remove duplacate elements from start
+            while(b<e && nums[e]==nums[e-1])e--;//,, from end
             int mid=b+(e-b)/2;
             if(target==nums[mid]){
             return true;}
