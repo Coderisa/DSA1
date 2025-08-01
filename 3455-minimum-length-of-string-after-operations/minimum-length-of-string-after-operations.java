0d8560d3fdbedd8df624f35e0e2a1d5be23d32dd
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-08-01 14:49:43 +0530

    Time: 159 ms (5.19%) | Memory: 45.8 MB (98.42%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,3 +1,21 @@
+class Solution{
+public int minimumLength(String s) {
+    Map<Character, Integer> freq = new HashMap<>();
+    int del = 0;
+
+    for (char ch : s.toCharArray()) {
+        freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+
+        if (freq.get(ch) == 3) {
+            freq.put(ch, 1); // reduce count by 2
+            del += 2;
+        }
+    }
+
+    return s.length() - del;
+}}
+/*
+
 class Solution {
     public int minimumLength(String s) {
         int freq[]=new int[26];int del=0;
@@ -11,7 +29,8 @@ for(int i=0; i< s.length(); i++){
 }
 return (s.length()-del);
     }
-}
+} */
+//OR 
         /*
         int freq[]=new int[26];
         int r=0;
