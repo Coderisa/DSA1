77385a8ac594ba054d17cff6aa6851e1d622eb7e
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-09-14 21:37:46 +0530

    Time: 5 ms (31.60%) | Memory: 52.8 MB (6.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -5,34 +5,29 @@ class Solution {
          row=grid.length; col=grid[0].length;
         boolean vis[][]=new boolean[row][col];//false
 
-     int flag=0, count=0;
+     int  count=0;
        for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
-            if(grid[i][j]!=0){
-             int b=dfs(i,j,grid,vis,flag);
-             if(b==1)
+            if(grid[i][j]!='0' && !vis[i][j]){
+             dfs(i,j,grid,vis);
              count++;
-             flag=0;
-            }
-              
+            }   
         }
        }
        return count;
-
     }
 
- int  dfs(int r,int c,char[][]grid,boolean vis[][],int flag){
+ void dfs(int r,int c,char[][]grid,boolean vis[][]){
 
             if(r<0||r>=row||c<0||c>=col||vis[r][c]==true||grid[r][c]=='0')
-            return 0;
-
-            vis[r][c]=true;
-            flag=1;
+            return ;
+          
+            vis[r][c]=true;  
             int adjList[][]={{r-1,c},{r,c-1},{r+1,c},{r,c+1}};
             for(int neighbour[]: adjList){
-                dfs(neighbour[0],neighbour[1],grid,vis,flag);
+                dfs(neighbour[0],neighbour[1],grid,vis);
             }
-            return flag;
+            
         }//dfs
 
 }
