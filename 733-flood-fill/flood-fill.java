3be7c9b497d0d53a52674528ad9fab91d907aab6
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-09-14 18:49:03 +0530

    Time: 1 ms (45.61%) | Memory: 45.5 MB (5.47%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,3 +1,4 @@
+/* DFS 
 class Solution {
     int rows,cols;//instance variable can be acced throuhout the class
     void dfs(int row, int col,int newColor, int curColor, int image[][]){
@@ -21,4 +22,40 @@ class Solution {
         dfs(sr,sc,color,image[sr][sc],image);
         return image;
     }
-}
+}*/
+//BFS -----------
+class Solution{
+    int rows;
+    int cols;
+public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        rows=image.length;
+        cols=image[0].length;
+        int curColor=image[sr][sc];
+        Queue<int[]> q=new LinkedList<>();//Queue<String> queue = new LinkedList<>();
+        q.offer(new int[]{sr,sc});
+        image[sr][sc]=color;//fill/ visited
+
+
+        while(!q.isEmpty()){
+            int node[]=q.poll();//current index in queue
+            int row=node[0];
+            int col=node[1];
+
+            //next node to be visited
+            int adjList[][]={{row-1,col},{row,col+1},{row+1,col},{row,col-1}};
+
+            //adding neighbours to queue
+        for(int neighbour[]:adjList){
+              int r=neighbour[0];
+              int c=neighbour[1];
+            if(r< 0 || r>=rows||c<0|| c>=cols|| image[r][c]!=curColor|| image[r][c]==color)
+            continue;//q out of bound hence skip
+
+            q.offer(new int[]{r,c});
+            image[r][c]=color;//add neighbour to queue and mark it visited
+        }//for
+
+        }
+        return image;
+    }
+        }//class
