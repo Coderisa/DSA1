97c0e80f0a9e1d22f6bcf7f88acce37754b847fe
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-10-21 21:11:56 +0530

    Time: 18 ms (79.97%) | Memory: 51.1 MB (67.16%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Base case: empty t can be formed from any prefix of s in exactly one way
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    // Match: include or exclude current character
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    // No match: exclude current character
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
