9c2f58ec573a93437d5171f33148a35b9da04923
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2025-10-21 23:51:24 +0530

    Time: 3 ms (63.92%) | Memory: 46.1 MB (49.39%) - LeetSync

---
+++ 341-flatten-nested-list-iterator/flatten-nested-list-iterator.java
@@ -0,0 +1,32 @@
+public class NestedIterator implements Iterator<Integer> {
+    private Deque<NestedInteger> stack;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new ArrayDeque<>();
+        // Push elements in reverse order so we can process them left to right
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            NestedInteger top = stack.peek();
+            if (top.isInteger()) {
+                return true;
+            }
+            stack.pop();
+            List<NestedInteger> list = top.getList();
+            for (int i = list.size() - 1; i >= 0; i--) {
+                stack.push(list.get(i));
+            }
+        }
+        return false;
+    }
+}
