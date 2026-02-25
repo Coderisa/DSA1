017afcd501eec64b0f0ce824c3a741b8cd2e509a
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-02-25 14:57:16 +0530

    Time: 17 ms (22.46%) | Memory: 46.4 MB (83.69%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -1,3 +1,34 @@
+//T- o(N)
+//S-o(1)-->CONSTAT AS ONLY 2 VARIABLES ARE USED T &F
+//faster approach as it avoids map overhead
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int i= 0, j=0, countF=0, countT=0, max=0;
+        while( j < answerKey.length()){
+            if (answerKey.charAt(j) == 'F') countF++;
+
+            while(countF > k){
+                if (answerKey.charAt(i) == 'F') countF--;
+                i++;
+            }
+            max=Math.max(max, j-i+1);
+            j++;
+        }
+  i= 0; j=0;
+         while( j < answerKey.length()){
+            if (answerKey.charAt(j) == 'T') countT++;
+
+            while(countT > k){
+                if (answerKey.charAt(i) == 'T') countT--;
+                i++;
+            }
+            max=Math.max(max, j-i+1);
+            j++;
+        }
+        return max;
+    }
+}
+/*
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
         HashMap<Character , Integer> mp =new HashMap<>();
@@ -11,12 +42,48 @@ class Solution {
     mp.put(left, mp.get(left) - 1);
     i++;
 }
-
-
              max=Math.max(max, j-i+1);
         }//for
         return max;
     }
 }
+------------------------------------------------------------------------------
+//Approach-1 (Simple Brute Force) - O(n * 2^n)
+class Solution {
+public:
+    int ans = 0;
+    int n;
+    
+    void findMax(string answerKey) {
+        int length = 0;
+        
+        int i = 0;
+
+        while(i < n) {
+            
+            if(answerKey[i] == 'T') {
+                length = 1;
+                i++;
+                while(i < n && answerKey[i] == 'T') {
+                    length++;
+                    i++;
+                }
+                ans = max(ans, length);
+            } else {
+                length = 1;
+                i++;
+                while(i < n && answerKey[i] == 'F') {
+                    length++;
+                    i++;
+                }
+                ans = max(ans, length);
+            }
+            
+        }
+    }
+    
+
+*/
+
 
 
