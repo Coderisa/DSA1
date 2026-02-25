4e31ab4174e3efea61bbb42ae0498c8cc13a5e43
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-02-25 14:50:10 +0530

    Time: 63 ms (5.01%) | Memory: 47.2 MB (5.72%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -1,5 +1,27 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
+        HashMap<Character , Integer> mp =new HashMap<>();
+        int i=0 , j=0, max =0;
+        for(j=0; j< answerKey.length();j++){
+           char ch = answerKey.charAt(j); 
+           mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+
+           while (Math.min(mp.getOrDefault('T', 0), mp.getOrDefault('F', 0)) > k) {
+    char left = answerKey.charAt(i);
+    mp.put(left, mp.get(left) - 1);
+    i++;
+}
+
+
+             max=Math.max(max, j-i+1);
+        }//for
+        return max;
+    }
+}
+
+
+/*class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
         int i= 0, j=0, countF=0, countT=0, max=0;
         while( j < answerKey.length()){
             if (answerKey.charAt(j) == 'F') countF++;
@@ -24,25 +46,6 @@ class Solution {
         }
         return max;
     }
-}
-/*
-class Solution {
-    public int maxConsecutiveAnswers(String answerKey, int k) {
-        HashMap<Character , Integer> mp =new HashMap<>();
-        int i=0 , j=0, max =0;
-        for(j=0; j< answerKey.length();j++){
-           char ch = answerKey.charAt(j); 
-           mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+}*/
 
-            while(Math.min ( mp.get('T'), mp.get('F') ) >k){
-             char left = answerKey.charAt(i);
-              mp.put(left, mp.get(left) - 1);
-                i++;
-            }
 
-             max=Math.max(max, j-i+1);
-        }//for
-        return max;
-    }
-}
-*/
