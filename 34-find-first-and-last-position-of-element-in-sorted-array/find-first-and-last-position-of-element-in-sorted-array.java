312069aed8279f8581016701c795cc10384a8658
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-02-28 13:56:34 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (71.68%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -38,3 +38,47 @@ class Solution {
 
     }
 }
+/*
+MORE BETTER VERSION
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int lb = findBound(nums, target, true);  // left bound
+        if (lb == -1) return new int[]{-1, -1};  // target not found
+        int up = findBound(nums, target, false); // right bound
+        return new int[]{lb, up};
+    }
+
+    private int findBound(int[] nums, int target, boolean isLeft) {
+        int b = 0, e = nums.length - 1, ans = -1;
+        while (b <= e) {
+            int mid = (b + e) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                if (isLeft) e = mid - 1;
+                else b = mid + 1;
+            } else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                b = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
+*/
+/*BRUTE FORCE
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int lb = -1, up = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (lb == -1) lb = i; // first occurrence
+                up = i;               // last occurrence (keeps updating)
+            }
+        }
+        
+        return new int[]{lb, up};
+    }
+}
+*/
