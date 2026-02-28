d039b7ac9911e0dc23a5acb93740bea13643b105
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-02-28 16:46:26 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (78.58%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        // Check if l is a valid single element
+         if ((l == 0 || nums[l] != nums[l - 1]) && (l == nums.length - 1 || nums[l] != nums[l + 1])) { 
+            return nums[l]; } 
+        return -1; // Return -1 if no single element is found
+        
+    }
+}
+/*Intuition
+Let's say the element we are looking for is the target. On observation, we notice that if the index of an element is odd, then its previous element at index -1 should be equal to it. Similarly, if the index of the element is even, then the element next to it or at index +1 should be equal to it. When this condition is not met, it means we have encountered the target.
+
+Approach
+Check if the index of mid is odd. If it is, then the element at mid - 1) should be equal to the element at mid. If the index is even, then the element at mid + 1 should be equal to the element at mid. If either condition is true, then the pivot has not occurred. In this case, set low = mid + 1.
+
+2)If the above condition is false, it means the target has already occurred, or the current element is the target itself. In this case, set high = mid.
+
+3)Return nums[l]
+
+Complexity
+Time complexity:O(log n)
+
+Space complexity:
+O(1) notice the array need not be sorted for this approach to work/*
+*/
