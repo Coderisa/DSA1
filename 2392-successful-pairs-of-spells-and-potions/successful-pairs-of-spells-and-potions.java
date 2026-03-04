04e57d993d58456c154615a27f74faeb93935a17
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-03-04 20:32:03 +0530

    Time: 48 ms (90.83%) | Memory: 146.7 MB (86.33%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -1,3 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int m = spells.length;
+        int n = potions.length;
+
+        // Sort potions so we can binary search
+        Arrays.sort(potions);
+        int maxPotion = potions[n - 1];
+
+        int[] answer = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int spell = spells[i];
+            long minPotion = (long) Math.ceil((1.0 * success) / spell);
+
+            // If even the strongest potion can't meet the requirement
+            if (minPotion > maxPotion) {
+                answer[i] = 0;
+                continue;
+            }
+
+            // Find the first potion >= minPotion
+            int index = lowerBound(potions, (int) minPotion);
+            answer[i] = n - index;
+        }
+
+        return answer;
+    }
+
+    // Custom binary search to find lower bound
+    private int lowerBound(int[] arr, int target) {
+        int l = 0, r = arr.length;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
+    }
+}
+
+/*
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         int pairs[]=new int[spells.length];
@@ -19,3 +69,4 @@ class Solution {
         return pairs;
     }
 }
+*/
