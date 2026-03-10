a3bc5d381aad3d894b2aed6fb5498afe9068f70e
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-03-10 18:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (70.93%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -18,3 +18,15 @@ public class Solution {
         return true;
     }
 }
+/*
+class Solution {
+    public boolean detectLoop(Node head) {
+        Node slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}*/
