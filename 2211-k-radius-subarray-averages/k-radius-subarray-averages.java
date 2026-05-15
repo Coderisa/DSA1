3e526f4fabe82559264e80b444091d524711347c
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-05-16 01:31:18 +0530

    Time: 7 ms (28.15%) | Memory: 175.6 MB (33.62%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -1,37 +1,29 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        int n = nums.length;
-        int[] ans = new int[n];
+        
+         int []ans = new int[nums.length];
+        // initialize with -1
         Arrays.fill(ans, -1);
+        
+        int l=0,r=2*k;long  sum=0;
+       
+        if (2*k+1 > nums.length) return ans; // no averages possible
 
-        int windowSize = 2 * k + 1;
-        if (windowSize > n) return ans; // no averages possible
-
-        long sum = 0;
-        // initial window sum
-        for (int idx = 0; idx < windowSize; idx++) {
-            sum += nums[idx];
+        for(int i =0 ;i< 2*k +1;i++){
+            sum+=nums[i];
         }
-
-        int l = 0;
-        int r = windowSize - 1;
-        int i = k;
-
-        // sliding window using while loop
-        while (r < n) {
-            ans[i] = (int)(sum / windowSize);
-
-            // move window forward
-            sum -= nums[l];
-            l++;
-            r++;
-            i++;
-
-            if (r < n) {
-                sum += nums[r];
-            }
+        int i =k;// k ke pahle wale sab 0 honge
+        
+        while( r <nums.length ){
+           
+           ans[i]= (int) (sum/(2*k+1) );
+            sum-=nums[l];
+           i++;l++;r++;
+           if ( r < nums.length) {
+            sum+=nums[r];}
+        
         }
-
+       
         return ans;
     }
 }
