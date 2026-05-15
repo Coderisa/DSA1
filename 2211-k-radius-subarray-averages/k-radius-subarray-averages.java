cbe50c76f80bf2c731b8a5ab13c30a214bc85b53
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-05-16 01:32:21 +0530

    Time: 6 ms (70.00%) | Memory: 175.5 MB (33.62%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -1,29 +1,27 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        
-         int []ans = new int[nums.length];
-        // initialize with -1
+        int[] ans = new int[nums.length];
         Arrays.fill(ans, -1);
         
-        int l=0,r=2*k;long  sum=0;
-       
-        if (2*k+1 > nums.length) return ans; // no averages possible
-
-        for(int i =0 ;i< 2*k +1;i++){
-            sum+=nums[i];
-        }
-        int i =k;// k ke pahle wale sab 0 honge
+        long sum = 0;
+        int windowSize = 2 * k + 1;
+        if (windowSize > nums.length) return ans;
         
-        while( r <nums.length ){
-           
-           ans[i]= (int) (sum/(2*k+1) );
-            sum-=nums[l];
-           i++;l++;r++;
-           if ( r < nums.length) {
-            sum+=nums[r];}
-        
+        for (int i = 0; i < windowSize; i++) {
+            sum += nums[i];
         }
-       
+        
+        int l = 0, r = 2 * k, i = k;
+        while (r < nums.length) {
+            ans[i] = (int)(sum / windowSize);   // ✅ division before cast
+            sum -= nums[l];
+            l++;
+            r++;
+            i++;
+            if (r < nums.length) {
+                sum += nums[r];
+            }
+        }
         return ans;
     }
 }
