c6a3da2aab33c5e96d4e934f5ab0f5dd3c371162
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-05-25 19:58:42 +0530

    Time: 3 ms (88.67%) | Memory: 44 MB (85.36%) - LeetSync

--- Sliding_window/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ Sliding_window/2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -1,15 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int numOfPairs(String[] nums, String target) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            
-            for(int j=0;j<nums.length;j++){
-                if(i!=j){
-               String m= nums[i]+nums[j];
-               if(m.equals(target)) c++; }
+        Map<String, Integer> freq = new HashMap<>();
+        for (String s : nums) {
+            freq.put(s, freq.getOrDefault(s, 0) + 1);
+        }
 
+        int count = 0;
+        for (String prefix : nums) {
+            if (target.startsWith(prefix)) {
+                String suffix = target.substring(prefix.length());
+                if (freq.containsKey(suffix)) {
+                    count += freq.get(suffix);
+                    // avoid counting same index twice
+                    if (prefix.equals(suffix)) {
+                        count--; 
+                    }
+                }
             }
         }
-        return c;
+        return count;
     }
 }
