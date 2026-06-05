d05fd3608bc92d085f2d3a48e97b557e702f0496
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-06-05 07:57:25 +0530

    Time: 3 ms (27.89%) | Memory: 49.6 MB (10.36%) - LeetSync

--- Sliding_window/547-number-of-provinces/number-of-provinces.java
+++ Sliding_window/547-number-of-provinces/number-of-provinces.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int findCircleNum(int[][] isConnected) {
          ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
@@ -15,6 +16,7 @@ class Solution {
                 }
             }
         }
+        
       
        
         boolean vis[]=new boolean[V];
@@ -35,4 +37,53 @@ class Solution {
               dfs(adj,vis,v);
        }
     }
+}*/
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+         ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+
+        int r=isConnected.length;int V =r; 
+        int c=isConnected[0].length;
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i!=j){
+                    if(isConnected[i][j]==1) 
+                      adj.get(i).add(j);
+                }
+            }
+        }
+        
+      
+       
+        boolean vis[]=new boolean[V];
+        int co=0;
+        
+
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+               bfs(adj,vis,i);
+               co++;
+            }
+        }
+        return co;
+    }
+    private void bfs(  ArrayList<ArrayList<Integer>>adj,boolean vis[],int u){
+      Queue<Integer>q=new LinkedList<>();
+        q.offer(u);
+       vis[u]=true;
+
+          while(!q.isEmpty()){
+           int n=q.poll();
+             for(int v:adj.get(n)){
+           if(!vis[v]) {
+                q.add(v);
+                vis[v]=true; }
+            
+       }
+    }
+}
 }
