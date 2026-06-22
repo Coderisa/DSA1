bb508ca2220a1d1fb9e627a813782c8b757150b2
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-06-22 20:59:31 +0530

    Time: 33 ms (85.80%) | Memory: 78.4 MB (79.63%) - LeetSync

--- Sliding_window/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
+++ Sliding_window/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.java
@@ -1,5 +1,98 @@
 class Solution {
     public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        long result = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            int diff = nums[i] - i;
+            int totalPairsTillIndex = i;
+            int goodPairs = mp.getOrDefault(diff, 0);
+            
+            result += (totalPairsTillIndex - goodPairs);
+            
+            mp.put(diff, goodPairs + 1);
+        }
+        
+        return result;
+    }
+}
+/*
+//Approach-1 (Brute Force)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        long result = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (j - i != nums[j] - nums[i]) {
+                    result++;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
+
+//Approach-2 (Using hashmap)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] - i;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 1);
+
+        for (int j = 1; j < n; j++) {
+            int countOfNumsj = map.getOrDefault(nums[j], 0);
+            int totalNumsBeforej = j;
+            int badPairs = totalNumsBeforej - countOfNumsj;
+            result += badPairs;
+            
+            map.put(nums[j], countOfNumsj + 1);
+        }
+
+        return result;
+    }
+}
+
+//Approach-3 (Using hashmap)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        long result = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            int diff = nums[i] - i;
+            int totalPairsTillIndex = i;
+            int goodPairs = mp.getOrDefault(diff, 0);
+            
+            result += (totalPairsTillIndex - goodPairs);
+            
+            mp.put(diff, goodPairs + 1);
+        }
+        
+        return result;
+    }
+}
+
+
+class Solution {
+    public long countBadPairs(int[] nums) {
         HashMap<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int key = nums[i] - i;
@@ -15,3 +108,22 @@ class Solution {
         return c / 2;                      // divide by 2!
     }
 }
+/*
+   class Solution {
+    public long countBadPairs(int[] nums) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = nums[i] - i;
+            mp.put(key, mp.getOrDefault(key, 0) + 1);
+        }
+        
+        long n = nums.length;
+        long totalPairs = n * (n - 1) / 2;
+        long goodPairs = 0;
+        for (int freq : mp.values()) {
+            goodPairs += (long) freq * (freq - 1) / 2;
+        }
+        return totalPairs - goodPairs;
+    }
+}
+*/
