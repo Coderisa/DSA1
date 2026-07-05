f4f22f9fbc73ba0db1f317f961c9af399001f455
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-07-06 01:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (32.59%) - LeetSync

--- Sliding_window/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ Sliding_window/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -23,23 +23,25 @@
 }//TIP: Inorder traversal of a Binary Search Tree is always sorted.
 */
 
+// we hv used In-order traversal just becoz we know that using it we 'll have to just traverse the sorted tree values as it traveses in sorted order;
+//varna pahle sort karna padta tree ko then ans ata.-O(nlogn)
 
 class Solution {
     private int count = 0;
-    int result =-1;//we r using a neg value here as we know from the constraints that node.val can never be neg
+    //int result =-1;//we r using a neg value here as we know from the constraints that node.val can never be neg
     
 
     public int kthSmallest(TreeNode root, int k) {
         if(root == null) return -1;//to indicate that kth abhi tak nhi mila hai
 
-       result= kthSmallest(root.left,k);//we r not checking if (root.left!=null) as wven if it is null then it will return -1 not go further
+    int   result= kthSmallest(root.left,k);//we r not checking if (root.left!=null) as wven if it is null then it will return -1 not go further
        if(result != -1) return result;
 
        if(count +1 ==k)  return root.val;
        else 
            count++;//update count
         
-        result = kthSmallest(root.right,k);
+       result = kthSmallest(root.right,k);
         if(result != -1) return result;
 
         return result;
