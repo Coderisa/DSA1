f665067730ccd8f488825b7c5d35ecbc7c75b4d9
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-07-11 04:11:32 +0530

    Time: 19 ms (91.45%) | Memory: 54.1 MB (19.87%) - LeetSync

--- Sliding_window/1250-longest-common-subsequence/longest-common-subsequence.java
+++ Sliding_window/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -16,6 +16,8 @@
     }
 }
 */
+
+/* RECURSON+MEMOIZATION
 import java.util.Arrays;
 
 class Solution {
@@ -38,5 +40,25 @@ class Solution {
             Arrays.fill(t[i], -1);
         }
         return solve(text1, text2, 0, 0);
+    }
+}
+*/
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        
+        // Fill table from bottom-right to top-left
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
