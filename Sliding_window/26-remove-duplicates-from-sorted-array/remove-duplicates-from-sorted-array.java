820b1c98adb482668aa288292e269d9357b8a93a
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-07-13 02:14:42 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (43.42%) - LeetSync

---
+++ Sliding_window/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,42 @@
+/*
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0,j=1;int l=nums.length;
+        while(j < l){
+            if (nums[i] != nums[j]){
+                nums[++i] = nums[j];
+            }
+            j++;
+        }
+        return (i+1);
+    }
+}*/
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0,j=1;int l=nums.length;
+        for(j=1;j<nums.length;j++){
+            if (nums[i] != nums[j]){
+                nums[++i] = nums[j];
+            }
+            
+        }
+        return (i+1);
+    }
+}
+
+        /*
+        if (nums.length == 0) return 0;
+        
+        int k = 1; // Start k at 1 since the first element is always unique
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i]; // Move unique elements to the front
+                k++;
+            }
+        }
+        return k; // k is the number of unique elements
+    }
+} */
+
+
+
